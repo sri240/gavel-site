9a0cd49944743a55ae6a4d379cad946da6e02f3a
art3m1s <anusrisaraf20@gmail.com>
2020-07-06 23:14:31 -0700

    added vpm contact

--- src/pages/register.js
+++ src/pages/register.js
@@ -16,7 +16,7 @@ const Register = () => (
     <div class="reginfo" style={{background:'#a13545', padding:"15px"}}><p class="title">INDUCTION PROCESS</p>
         <p class="filler" style={{lineHeight:"40px"}}>STEP 1: Fill out the form. <br></br>
         STEP 2: Once we let you know that you have been accepted, contact Savitha Setlur to obtain your manuals.<br></br>
-        STEP 3: Contact the Vice President of Membership (Sohum Tiwary) to get added to the Google Group and WhatsApp group chat for communication.<br></br>
+        STEP 3: Contact the Vice President of Membership (Sohum Tiwary: sohum.tiwary@gmail.com) to get added to the Google Group and WhatsApp group chat for communication.<br></br>
         STEP 4: Once you are added to the google group, please send an email listing your goals for the Gavel Club to the google group. Under the recipients section, type “Gavel Club” and press enter. <br></br>
         ON OUR END: You will be added to the Google Group and WhatsApp chat for club communications and access to the Roles Sign Up Sheet. You will have received your manuals and will be added to the active members list so that you may receive points.</p></div>
     <p><br></br></p>
